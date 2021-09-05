3081a2fc13216232e6bde70d482b3751e68f1ef8
Ashok Kumar <ashokkumar200194@gmail.com>
2021-09-05 19:35:33 +0530

    week 3

--- Algorithm_Tool_Box/Week_3/Change.java
+++ Algorithm_Tool_Box/Week_3/Change.java
@@ -2,28 +2,18 @@ import java.util.Scanner;
 
 public class Change {
     private static int getChange(int m) {
-        //write your code here
-        /*int temp = 0, count = 0;
-        while (m > 0){
-            if (m >= 10){
-                count = count + 1 ;
-                m = m - 10;
-            }
-            else if (m >= 5){
-                count = count + 1;
-                m = m - 5;
-            }
-            else{
-                count = count + 1 ;
-                m = m - 1;
-            } 
-        }*/
+        // write your code here
+        /*
+         * int temp = 0, count = 0; while (m > 0){ if (m >= 10){ count = count + 1 ; m =
+         * m - 10; } else if (m >= 5){ count = count + 1; m = m - 5; } else{ count =
+         * count + 1 ; m = m - 1; } }
+         */
         if (m <= 0)
             return -1;
         else if (m == 10 || m == 5 || m == 1)
             return 1;
         else
-            return m/10+(m%10)/5+m%5;
+            return m / 10 + (m % 10) / 5 + m % 5;
     }
 
     public static void main(String[] args) {
@@ -33,4 +23,3 @@ public class Change {
 
     }
 }
-
--- Algorithm_Tool_Box/Week_3/FractionalKnapsack.java
+++ Algorithm_Tool_Box/Week_3/FractionalKnapsack.java
@@ -3,42 +3,43 @@ import java.util.Scanner;
 public class FractionalKnapsack {
     private static double getOptimalValue(int capacity, int[] values, int[] weights) {
         double value = 0;
-        //write your code here
+        // write your code here
         int n = values.length;
         double[] arr = new double[n];
-        for(int i=0; i<n;i++){
-            arr[i]=(double)values[i]/(double)weights[i];
+        double temp = 0.0;
+        int temp1 = 0;
+        for (int i = 0; i < n; i++) {
+            arr[i] = (double) values[i] / (double) weights[i];
         }
-        for(int i = 0; i < n-1 ; i++){
-            for (int j = i+1 ; j < n-i-1; j++){
-                if(arr[j] < arr[j+1])
-                {
-                    arr[j] = arr[j] + arr[j+1];
-                    arr[j+1] = arr[j] - arr[j+1];
-                    arr[j] = arr[j] -arr[j+1];
-                    values[j]=values[j]+ values[j+1];
-                    values[j+1] = values[j] - values [j+1];
-                    values[j] = values[j] -values [j+1];
-                    weights[j]=weights[j]+ weights[j+1];
-                    weights[j+1] = weights[j] - weights [j+1];
-                    weights[j] = weights[j] -weights [j+1];
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                    temp1 = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = temp1;
+                    temp1 = weights[j];
+                    weights[j] = weights[j + 1];
+                    weights[j + 1] = temp1;
+
                 }
             }
         }
-        int k=0;
-        while (capacity !=0 && k < n){
-            if (capacity >= weights[k]){
+        int k = 0;
+        while (capacity != 0 && k < n) {
+            if (capacity >= weights[k]) {
                 capacity = capacity - weights[k];
                 value = value + values[k];
                 k++;
-            }
-            else{
-                value = value + (arr[k]*capacity);
+            } else {
+                value = value + (arr[k] * capacity);
                 capacity = 0;
             }
         }
-        return value ;
-    
+        return value;
+
     }
 
     public static void main(String args[]) {
@@ -53,4 +54,4 @@ public class FractionalKnapsack {
         }
         System.out.println(getOptimalValue(capacity, values, weights));
     }
-} 
+}
