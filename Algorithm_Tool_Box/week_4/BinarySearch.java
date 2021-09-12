b140c3fff792b4bdfdbe5aa712489d286f46286e
Ashok Kumar <ashokkumar200194@gmail.com>
2021-09-12 20:48:48 +0530

    week 4

---
+++ Algorithm_Tool_Box/week_4/BinarySearch.java
@@ -0,0 +1,49 @@
+import java.util.*;
+import java.util.ArrayList;
+
+public class BinarySearch{
+    private static int BinarySearch(ArrayList a,int low,int high,int key){
+        /*int mid = (low+high)/2;
+        int p = (Integer)a.get(mid);
+        if (high<low)
+            return -1;
+        if (key == p)
+            return mid;
+        else if (key<p) 
+            return BinarySearch(a, low, mid-1, key);
+        else
+            return BinarySearch(a, mid+1, high, key);
+            */
+        while(low <= high){
+            int mid = low+(high-low/2);
+            if (key == (Integer)a.get(mid))
+                return mid;
+            else if ( key < (Integer)a.get(mid))
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return -1;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt(),i;
+        ArrayList<Integer> arr = new ArrayList<Integer>(n);
+        for(i = 0; i < n; i++){
+            arr.add(scan.nextInt());
+        }
+        int k = scan.nextInt();
+        ArrayList<Integer> karr = new ArrayList<Integer>(k);
+        for(i = 0; i < k ; i++){
+            karr.add(scan.nextInt());
+        }
+        int res;
+        ArrayList<Integer> results = new ArrayList<Integer>(k);
+        for(i = 0 ; i < k ; i++){
+            res = BinarySearch(arr,0,arr.size()-1,karr.get(i));
+            results.add(res);
+        }
+        for(i=0;i<n;i++)
+        System.out.printf("%d ",results.get(i));
+    }
+}
---
+++ Algorithm_Tool_Box/week_4/MajorityElement.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+
+public class MajorityElement{
+    private static final Scanner scan = new Scanner(System.in);
+    private static boolean MajorityElementPresent(int[] arr){
+        Map<Integer,Integer> frequencies = getFrequency(arr);
+        for(int frequency: frequencies.values()){
+            if (frequency > arr.length/2 )
+            return true;
+        }
+        return false;
+    }
+    private static Map<Integer,Integer> getFrequency(int[] arr){
+        Map<Integer, Integer> frequencies = new HashMap<>();
+        for(int element: arr)
+            frequencies.put(element, frequencies.getOrDefault(element,0)+1);
+        return frequencies;
+    }
+    public static void main(String args[]) {
+        int n = scan.nextInt();
+        int[] arr = new int[n];
+        for(int i=0; i < n; i++)
+            arr[i] = scan.nextInt();
+        System.out.println(MajorityElementPresent(arr)? 1 : 0);
+        
+    }
+}
