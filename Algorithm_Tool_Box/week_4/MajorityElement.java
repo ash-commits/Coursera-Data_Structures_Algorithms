acc15be394f768ca99bf99caebfb18946b647b4b
Ashok Kumar <ashokkumar200194@gmail.com>
2021-09-12 20:51:07 +0530

    week 4

--- Algorithm_Tool_Box/week_4/MajorityElement.java
+++ Algorithm_Tool_Box/week_4/MajorityElement.java
@@ -1,28 +1,32 @@
 import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-
-public class MajorityElement{
+public class MajorityElement {
     private static final Scanner scan = new Scanner(System.in);
-    private static boolean MajorityElementPresent(int[] arr){
-        Map<Integer,Integer> frequencies = getFrequency(arr);
-        for(int frequency: frequencies.values()){
-            if (frequency > arr.length/2 )
-            return true;
+
+    private static boolean MajorityElementPresent(int[] arr) {
+        Map<Integer, Integer> frequencies = getFrequency(arr);
+        for (int frequency : frequencies.values()) {
+            if (frequency > arr.length / 2)
+                return true;
         }
         return false;
     }
-    private static Map<Integer,Integer> getFrequency(int[] arr){
+
+    private static Map<Integer, Integer> getFrequency(int[] arr) {
         Map<Integer, Integer> frequencies = new HashMap<>();
-        for(int element: arr)
-            frequencies.put(element, frequencies.getOrDefault(element,0)+1);
+        for (int element : arr)
+            frequencies.put(element, frequencies.getOrDefault(element, 0) + 1);
         return frequencies;
     }
+
     public static void main(String args[]) {
         int n = scan.nextInt();
         int[] arr = new int[n];
-        for(int i=0; i < n; i++)
+        for (int i = 0; i < n; i++)
             arr[i] = scan.nextInt();
-        System.out.println(MajorityElementPresent(arr)? 1 : 0);
-        
+        System.out.println(MajorityElementPresent(arr) ? 1 : 0);
+
     }
 }
